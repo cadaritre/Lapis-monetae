b05fe89f5a2c10ce21ba7b6696cb2bfda3a16ba3
cadaritre <cadaritre@gmail.com>
2026-03-10 07:33:00 -0600

    bug fixes

--- consensus/src/processes/coinbase.rs
+++ consensus/src/processes/coinbase.rs
@@ -346,12 +346,27 @@ mod tests {
 
     #[test]
     fn calc_high_bps_total_rewards_delta() {
-        let legacy_cbm = create_legacy_manager();
-        let pre_deflationary_rewards = legacy_cbm.pre_deflationary_phase_base_subsidy * legacy_cbm.deflationary_phase_daa_score;
-        let total_rewards: u64 = pre_deflationary_rewards + SUBSIDY_BY_MONTH_TABLE.iter().map(|x| x * SECONDS_PER_MONTH).sum::<u64>();
+        let params = &SIMNET_PARAMS;
+        let target_total_sompi: u128 = 100_000_000u128 * (SOMPI_PER_KASPA as u128);
+        let pre_deflation_total: u128 = (params.pre_deflationary_phase_base_subsidy as u128) * (params.deflationary_phase_daa_score as u128);
+        let deflation_total: u128 = SUBSIDY_BY_MONTH_TABLE.iter().map(|v| (*v as u128) * (SECONDS_PER_MONTH as u128)).sum();
+        let scheduled_total: u128 = pre_deflation_total + deflation_total;
+        let emission_divisor: u64 = if scheduled_total <= target_total_sompi {
+            1
+        } else {
+            ((scheduled_total + target_total_sompi - 1) / target_total_sompi) as u64
+        };
+
+        let scaled_pre_deflationary_base = params.pre_deflationary_phase_base_subsidy.div_ceil(emission_divisor);
+        let pre_deflationary_rewards = scaled_pre_deflationary_base * params.deflationary_phase_daa_score;
+        let total_rewards: u64 =
+            pre_deflationary_rewards + SUBSIDY_BY_MONTH_TABLE.iter().map(|x| x.div_ceil(emission_divisor) * SECONDS_PER_MONTH).sum::<u64>();
         let testnet_11_bps = SIMNET_PARAMS.bps().upper_bound();
         let total_high_bps_rewards_rounded_up: u64 = pre_deflationary_rewards
-            + SUBSIDY_BY_MONTH_TABLE.iter().map(|x| (x.div_ceil(testnet_11_bps) * testnet_11_bps) * SECONDS_PER_MONTH).sum::<u64>();
+            + SUBSIDY_BY_MONTH_TABLE
+                .iter()
+                .map(|x| ((x.div_ceil(emission_divisor).div_ceil(testnet_11_bps)) * testnet_11_bps) * SECONDS_PER_MONTH)
+                .sum::<u64>();
 
         let cbm = create_manager(&SIMNET_PARAMS);
         let total_high_bps_rewards: u64 = pre_deflationary_rewards
